3ac347e499b2cdad3bda245a30be96e7f7c96d86
fouliex <fouliex@gmail.com>
2016-11-06 20:38:23 -0800

    Add a Builder pattern example

---
+++ src/main/java/designPatterns/builderpattern/App.java
@@ -0,0 +1,16 @@
+package designPatterns.builderpattern;
+
+/**
+ * Created by fouli on 11/6/2016.
+ */
+public class App {
+    public static void main(String[] args) {
+        User websiteUser = new User.Builder("bobMax", "bobMax@gmail.com").
+                firstName("Bob").
+                lastName("Max").
+                phoneNumber(5555).
+                address("Haiti").
+                build();
+        System.out.println(websiteUser);
+    }
+}
---
+++ src/main/java/designPatterns/builderpattern/User.java
@@ -0,0 +1,86 @@
+package designPatterns.builderpattern;
+
+/**
+ * Created by fouli on 11/6/2016.
+ */
+public class User {
+    /**
+     * Required
+     */
+    private String userName;
+    private String emailAddress;
+
+    /**
+     * Optional
+     */
+    private String firstName;
+    private String lastName;
+    private int phoneNumber;
+    private String address;
+
+    private User(Builder builder) {
+        firstName = builder.firstName;
+        lastName = builder.lastName;
+        userName = builder.userName;
+        emailAddress = builder.emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userName='" + userName + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        /**
+         * Required
+         */
+        private String userName;
+        private String emailAddress;
+
+        /**
+         * Optional
+         */
+        private String firstName;
+        private String lastName;
+        private int phoneNumber;
+        private String address;
+
+        public Builder(String userName, String emailAddress) {
+            this.userName = userName;
+            this.emailAddress = emailAddress;
+        }
+
+        public Builder firstName(String value) {
+            firstName = value;
+            return this;
+        }
+
+        public Builder lastName(String value) {
+            lastName = value;
+            return this;
+        }
+
+        public Builder phoneNumber(int value) {
+            phoneNumber = value;
+            return this;
+        }
+
+        public Builder address(String value) {
+            address = value;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
+
+
+}
