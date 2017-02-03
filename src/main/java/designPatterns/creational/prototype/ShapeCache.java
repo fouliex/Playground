2861b1ac2e700e2d4856412589cb3f5aa29c05fc
fouliex <fouliex@gmail.com>
2017-02-03 07:48:38 -0800

    Commit what I have so far for the prototype pattern example

---
+++ src/main/java/designPatterns/creational/prototype/ShapeCache.java
@@ -0,0 +1,10 @@
+package designPatterns.creational.prototype;
+
+import java.util.Hashtable;
+
+/**
+ * Created by fouli on 2/2/2017.
+ */
+public class ShapeCache {
+    private static Hashtable<String,Shape>shapeMap = new Hashtable<String,Shape>();
+}
