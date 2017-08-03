ff9a58313c9a16ebfe3cd9552faf26358190e05d
fouliex <fouliex@gmail.com>
2017-08-02 22:50:17 -0700

    Coderbyte TimeConvert initial commit

---
+++ src/main/java/coderbyte/easy/TimeConvert.java
@@ -0,0 +1,11 @@
+package coderbyte.easy;
+
+/**
+ * Have a function that take a number parameter and return the number of hours and minutes.
+ * For example if num =63 then the output should be 1:3.
+ * <p>
+ * Separate the number of hours and minutes with a colon.
+ * Created by fouli on 8/2/2017.
+ */
+public class TimeConvert {
+}
---
+++ src/test/java/coderbyte/easy/TimeConvertTest.java
@@ -0,0 +1,10 @@
+package coderbyte.easy;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by fouli on 8/2/2017.
+ */
+public class TimeConvertTest {
+
+}
