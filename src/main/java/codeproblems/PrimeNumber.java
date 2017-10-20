f471d4d3ddc4fd35cef6d2439427dac43e2f60ed
George Fouche <fouliex@gmail.com>
2017-10-20 10:24:45 -0700

    Add PrimeNumber and PermutationStep

---
+++ src/main/java/codeproblems/PrimeNumber.java
@@ -0,0 +1,28 @@
+package codeproblems;
+
+class PrimeNumber {
+    public boolean isPrime(int n) {
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+
+    public void primeList(int number){
+        for(int i = 2; i <= number; i++){
+            if(isPrime(i)){
+                System.out.println(i + " ");
+            }
+        }
+
+    }
+
+    public static void main(String args[]) throws Exception {
+        PrimeNumber p = new PrimeNumber();
+        p.primeList(9);
+
+
+    }
+}
