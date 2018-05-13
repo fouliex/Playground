39f19bbc660c2eb3ffd9cee7d3637fb5971f2de3
George Fouche <fouliex@gmail.com>
2018-05-12 20:51:19 -0700

    Add Leetcode problems

---
+++ src/main/java/leetcode/PlusOne.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class PlusOne {
+
+    public int[] plusOne(int[] digits) {
+
+        int n = digits.length;
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] newNumber = new int[n + 1];
+        newNumber[0] = 1;
+
+        return newNumber;
+    }
+}
